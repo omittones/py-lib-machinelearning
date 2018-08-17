65402ea60b363d4bc328420e3bf853b2e3fd4c94
Augustin Juricic <augustin.juricic@gmail.com>
2018-08-17 13:19:28 +0200

    Development.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 import gym
-env = gym.make('CartPole-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+
+def main():
+    env = gym.make('CartPole-v0')
+    for i_episode in range(20):
+        print(f"Starting episode {i_episode}")
+        observation = env.reset()
+        t = 0
+        while True:
+            env.render()
+            print(observation)
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            t += 1
+            if done:
+                print(f"Episode finished after {t+1} timesteps")
+                break
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
