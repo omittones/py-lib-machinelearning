50aecea30aadb2a0c2e56259f86f07d87e826ed6
Augustin Juricic <augustin.juricic@gmail.com>
2019-05-12 21:28:01 +0200

    Working XOR model

--- src/learning.py
+++ src/learning.py
@@ -1,4 +1,4 @@
-from keras.layers import Input, Dense, Softmax
+from keras.layers import Input, Dense, Softmax, LeakyReLU
 from keras.models import Model
 from keras.optimizers import SGD
 from keras import losses
@@ -7,11 +7,14 @@ import numpy as np
 def main():
 
     inputs = Input(shape=(2,), dtype='float32')
-    x = Dense(10, activation='relu')(inputs)
-    x = Dense(2, activation='relu')(x)
+    x = Dense(10)(inputs)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = Dense(10)(inputs)
+    x = LeakyReLU(alpha=0.3)(x)
+    x = Dense(2)(x)
     outputs = Softmax()(x)
 
-    sgd = SGD(lr=0.1, momentum=0.0, decay=0.0, nesterov=False)
+    sgd = SGD(lr=0.05, momentum=0.0, decay=0.0, nesterov=False)
 
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(optimizer=sgd, loss=losses.categorical_crossentropy, metrics=['accuracy'])
@@ -24,13 +27,14 @@ def main():
         classes[i, 0] = 0 if x != y else 1
         classes[i, 1] = 1 if x != y else 0
 
-    model.fit(x=data, y=classes, epochs=20, verbose=2, batch_size=10)
+    model.fit(x=data, y=classes, epochs=50, verbose=2, batch_size=10)
 
     while True:
         x = input('Enter x,y: ')
         if x:
             x = [[float(i) for i in x.split(',')]]
             x = np.array(x, dtype='float32')
-            print(model.predict(x))
+            prediction = model.predict(x)
+            print(prediction, '--> class:', prediction.argmax())
         else:
             break
